d2285ed59a3de2ec1b3fcf9dff78effe835139f1
Atticus C <atticus@softwarefuze.com>
2022-02-06 18:55:39 -0800

    Add main content to homepage

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
-import { Nav, LockModel } from "../components";
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from "react";
+import { LockModel, Nav } from "../components";
+import { Canvas } from "@react-three/fiber";
+import React, { Suspense } from "react";
 
 const Home: NextPage = () => {
   return (
@@ -19,10 +19,7 @@ const Home: NextPage = () => {
       <main className={styles.banner}>
         <div className={styles.content}>
           <div className={styles.heading}>
-            <h1>
-              <span>Connect</span> <span>Online,</span> <span>with</span>{" "}
-              <span>Privacy</span>
-            </h1>
+            <h1>Connect with others online, with privacy</h1>
           </div>
 
           <div className={styles.model}>
@@ -53,7 +50,11 @@ const Home: NextPage = () => {
               <pointLight position={[-1, 0, 2]} intensity={0.075} />
               <pointLight position={[-1, -2, 2]} intensity={0.075} />
               <Suspense fallback={null}>
-                <LockModel rotation={[0, 30.08, 0]} position={[0, -1.5, 0]} scale={[1.25, 1.25, 1.25]} />
+                <LockModel
+                  rotation={[0, 30.08, 0]}
+                  position={[0, -1.5, 0]}
+                  scale={[1.25, 1.25, 1.25]}
+                />
               </Suspense>
             </Canvas>
           </div>
@@ -72,7 +73,49 @@ const Home: NextPage = () => {
           >
           </path>
         </svg>
+
+        <a className={styles.scroll} href="#about">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"><path stroke="#FF8A65" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" d="M19.92 8.95l-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"></path></svg>
+        </a>
       </main>
+
+      <section className={styles.section}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          className={styles.topWave}
+        >
+          <path
+            fill="rgb(52, 138, 167)"
+            fillOpacity="1"
+            d="M0,288L16,277.3C32,267,64,245,96,240C128,235,160,245,192,250.7C224,256,256,256,288,229.3C320,203,352,149,384,122.7C416,96,448,96,480,85.3C512,75,544,53,576,69.3C608,85,640,139,672,138.7C704,139,736,85,768,85.3C800,85,832,139,864,133.3C896,128,928,64,960,64C992,64,1024,128,1056,165.3C1088,203,1120,213,1152,208C1184,203,1216,181,1248,170.7C1280,160,1312,160,1344,176C1376,192,1408,224,1424,240L1440,256L1440,0L1424,0C1408,0,1376,0,1344,0C1312,0,1280,0,1248,0C1216,0,1184,0,1152,0C1120,0,1088,0,1056,0C1024,0,992,0,960,0C928,0,896,0,864,0C832,0,800,0,768,0C736,0,704,0,672,0C640,0,608,0,576,0C544,0,512,0,480,0C448,0,416,0,384,0C352,0,320,0,288,0C256,0,224,0,192,0C160,0,128,0,96,0C64,0,32,0,16,0L0,0Z"
+          >
+          </path>
+
+        </svg>
+
+        <div className={styles.sectionContent}>
+          <h1 className={styles.sectionHeading} id="#about">About ScrapBook</h1>
+          <p className={styles.sectionDescription}>ScrapBook is an online social media platform that allows you to connect with your friends and communities, from all across the platform. We've heard about the problems of data collection and other complaints from other social media platforms, and we fixed them all up, into an amazing web app for you!</p>
+        </div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.sectionContent}>
+          <h1 className={styles.sectionHeading}>Why Us?</h1>
+          <p className={styles.sectionDescription}>We're open source, and we don't collect your personal data.</p>
+          <p className={styles.sectionDescription}>Although, if you want us to recommend communities and other users, opt-in to the interest-related suggestion plan, where you input your interests, and we try and find users and communities you might like!</p>
+          <p className={styles.sectionDescription}>We encrypt all of your private information and DMs.</p>
+          <p className={styles.sectionDescription}>Discover and join, or create your own communities on ScrapBook!</p>
+        </div>
+      </section>
+
+      <section className={styles.signup}>
+        <h1>Well, what are you waiting for?</h1>
+        <p>
+          ✨ <Link href="/signup">Sign Up</Link> Today! ✨
+        </p>
+      </section>
     </>
   );
 };
