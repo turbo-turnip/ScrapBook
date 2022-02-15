140af55cad5885803331e0daf332757d8f60ff83
Atticus C <atticus@softwarefuze.com>
2022-02-15 06:20:39 -0800

    Added verify page

--- client/pages/verification-email/verify.tsx
+++ client/pages/verification-email/verify.tsx
@@ -3,19 +3,26 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Nav, AuthForm, FieldType } from "../../components";
 
+
 const Verify: NextPage = () => {
   const router = useRouter();
 
-  const verifyUser = async () => {
+  const verifyUser = async (inputs: Array<any>) => {
+    const address = new URL(window.location.href);
+    const searchParams = new URLSearchParams(address.search);
+
     const req = await fetch(backendPath + "/users/verify", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-
+        username: searchParams.get("u"),
+        code: searchParams.get("c")
       })
-    })
+    });
+    const res: ServerResponse = await req.json();
+    console.log(res);
   }
 
   useEffect(() => {
@@ -25,8 +32,6 @@ const Verify: NextPage = () => {
       router.push('/login');
       return;
     }
-
-
   }, []);
 
   return (
@@ -35,7 +40,7 @@ const Verify: NextPage = () => {
 
       <AuthForm heading="Verify" fields={[
         { label: "Password", placeholder: "Please enter your password", required: true, type: FieldType.PASS }
-      ]} />
+      ]} links={[]} submitHandler={verifyUser} />
     </>
   );
 }
